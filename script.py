9e1c8b7f6fcda9546199118821f963c200538a38
Dano Gayer <powdergod@gmail.com>
2026-01-30 11:14:29 -0700

    added comments and links to index and script reasoning and explinations to what is happening and what we can do for the project

--- script.py
+++ script.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-# Global valve states
+'''
+Global valve states
+this is going to have to change as we will need to keep the state in the database and update
+the DB to keep the record if everything crasses then reload form last state
+'''
 valve1 = False
 valve2 = False
 
@@ -20,9 +24,18 @@ def toggle(valve_id):
         valve2 = not valve2
 
     return jsonify({
+     '''   
+    {"v1": true, "v2": false} returns html response or what ever the vales are T or false
+     jsonify creates the JSON string and a flask response instead of complicating flask with a
+     JSON dump this does both at the same time and works better accross more browsers
+     '''
         "v1": valve1,
         "v2": valve2
     })
+'''
+https://www.geeksforgeeks.org/python/switch-case-in-python-replacement/
+this may be a route to go as well for each valve to go into diffrent states and what we need to do in each change
 
+'''
 if __name__ == "__main__":
     app.run(debug=True)
