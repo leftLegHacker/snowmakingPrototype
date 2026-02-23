f9cd2a55126d5ba5e4bb39292768c87c2fe20512
leftLegHacker <151093395+leftLegHacker@users.noreply.github.com>
2026-02-23 13:20:35 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,41 +1,10 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-'''
-Global valve states
-this is going to have to change as we will need to keep the state in the database and update
-the DB to keep the record if everything crasses then reload form last state
-'''
-valve1 = False
-valve2 = False
-
 @app.route("/")
-def index():
-    return render_template("index.html", v1=valve1, v2=valve2)
+def home():
+    return render_template("index.html")
 
-@app.route("/toggle/<int:valve_id>")
-def toggle(valve_id):
-    global valve1, valve2
-
-    if valve_id == 1:
-        valve1 = not valve1
-    elif valve_id == 2:
-        valve2 = not valve2
-
-    return jsonify({
-     '''   
-    {"v1": true, "v2": false} returns html response or what ever the vales are T or false
-     jsonify creates the JSON string and a flask response instead of complicating flask with a
-     JSON dump this does both at the same time and works better accross more browsers
-     '''
-        "v1": valve1,
-        "v2": valve2
-    })
-'''
-https://www.geeksforgeeks.org/python/switch-case-in-python-replacement/
-this may be a route to go as well for each valve to go into diffrent states and what we need to do in each change
-
-'''
 if __name__ == "__main__":
     app.run(debug=True)
